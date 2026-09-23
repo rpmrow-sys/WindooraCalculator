3b88730b64b7709156e7d9cbc9767dfdb551531b
rpmrow-sys <rpmrow@gmail.com>
2026-09-23 14:28:57 +0530

    Add MainActivity to load calculator HTML in WebView

---
+++ app/src/main/java/com/example/windooracalculator/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.windooracalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        webView.loadUrl("file:///android_asset/calculator.html");
+        setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
